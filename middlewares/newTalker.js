7d4928755d130dc6f3a766d4adf419878b6c9c95
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-12-07 20:40:21 -0300

    hum...

--- middlewares/newTalker.js
+++ middlewares/newTalker.js
@@ -1,10 +1,12 @@
 const fs = require('fs/promises');
 
-const regexData = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+const regexData =
+  /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
 
 const validateAut = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });  
+  if (!authorization)
+    return res.status(401).json({ message: 'Token não encontrado' });
   if (authorization !== '7mqaVRXJSp886CGr') {
     return res.status(401).json({ message: 'Token inválido' });
   }
@@ -14,20 +16,25 @@ const validateAut = (req, res, next) => {
 const validateName = (req, res, next) => {
   const { name } = req.body;
   if (!name || name === '') {
-    return res.status(400).json({ message: 'O campo "name" é obrigatório' })
-  };
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
   if (name.length < 3) {
-    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' })
-  };
+    return res
+      .status(400)
+      .json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
   next();
 };
 
 const validateAge = (req, res, next) => {
   const { age } = req.body;
-  if (!age || age === '') return res.status(400).json({ message: 'O campo "age" é obrigatório' });  
+  if (!age || age === '')
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
   if (age < 18) {
-    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' })
-  };
+    return res
+      .status(400)
+      .json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
   next();
 };
 
@@ -46,12 +53,21 @@ const newTalker = async (req) => {
   const newData = JSON.stringify(jsonRetorno);
   console.log(JSON.stringify(jsonRetorno));
   fs.writeFile('./talker.json', newData);
-}
+};
 
 const validateTalk = (req, res) => {
   const { talk } = req.body;
-  if (!talk || !talk.watchedAt || !talk.rate) return res.status(400).json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });  
-  if (!regexData.test(talk.watchedAt)) return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  if (!talk || !talk.watchedAt || !talk.rate)
+    return res
+      .status(400)
+      .json({
+        message:
+          'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+      });
+  if (!regexData.test(talk.watchedAt))
+    return res
+      .status(400)
+      .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
   if (!Number.isInteger(talk.rate) || talk.rate > 5 || talk.rate < 1) {
     return res.status(400).json({
       message: 'O campo "rate" deve ser um inteiro de 1 à 5',
