b5e47c12ecdb276d91500108b77db077e1dd53b6
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-12-07 16:28:58 -0300

    [requisito 2] resolucao

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs/promises");
 const getTalkers = require("./middlewares/getTalkers");
+const talkerIdFind = require("./middlewares/talkerIdFind");
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +12,8 @@ const PORT = "3000";
 
 // requisito 1
 app.get("/talker", getTalkers);
+// requisito 2
+app.get("/talker/:id", talkerIdFind);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get("/", (_request, response) => {
