f13b3f21f5b8821bbd64b1ca5222cac2cb2a8307
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-12-07 20:16:32 -0300

    lint novamente

--- middlewares/newTalker.js
+++ middlewares/newTalker.js
@@ -62,21 +62,21 @@ const newTalker = async () => {
   const newData = JSON.stringify(jsonRetorno);
   console.log(JSON.stringify(jsonRetorno));
   fs.writeFile('./talker.json', newData);
-}
+};
 
 const validateTalk = (req, res) => {
   const { talk } = req.body;
-  if (!talk || !talk.watchedAt || !talk.rate) {
-    return res.status(400).json({
-      message:
-        'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
-    });
-  }
-  if (!regexData.test(talk.watchedAt)) {
-    return res.status(400).json({
-      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
-    });
-  }
+  if (!talk || !talk.watchedAt || !talk.rate)
+    return res
+      .status(400)
+      .json({
+        message:
+          'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+      });
+  if (!regexData.test(talk.watchedAt))
+    return res
+      .status(400)
+      .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
   if (!Number.isInteger(talk.rate) || talk.rate > 5 || talk.rate < 1) {
     return res.status(400).json({
       message: 'O campo "rate" deve ser um inteiro de 1 à 5',
--- middlewares/tokenLogin.js
+++ middlewares/tokenLogin.js
@@ -2,26 +2,10 @@ const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
 
 const tokenLogin = (req, res) => {
   const { email, password } = req.body;
-  if (!email || email.length === 0) {
-    return res.status(400).json({
-      message: 'O campo "email" é obrigatório',
-    });
-  }
-  if (!regexEmail.test(email)) {
-    return res.status(400).json({
-      message: 'O "email" deve ter o formato "email@email.com"',
-    });
-  }
-  if (!password || password.length === 0) {
-    return res.status(400).json({
-      message: 'O campo "password" é obrigatório',
-    });
-  }
-  if (password.length < 6) {
-    return res.status(400).json({
-      message: 'O "password" deve ter pelo menos 6 caracteres',
-    });
-  }
+  if (!email || email.length === 0) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!regexEmail.test(email)) return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });  
+  if (!password || password.length === 0) return res.status(400).json({ message: 'O campo "password" é obrigatório' });  
+  if (password.length < 6) return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });  
   return res.status(200).json({
     token: '7mqaVRXJSp886CGr',
   });
