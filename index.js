c834d63d99874a987142864080842a982aa519e5
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-12-07 19:56:07 -0300

    [requisito 4] lint part

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const getTalkers = require("./middlewares/getTalkers");
-const talkerIdFind = require("./middlewares/talkerIdFind");
-const tokenLogin = require("./middlewares/tokenLogin");
+const express = require('express');
+const bodyParser = require('body-parser');
+const getTalkers = require('./middlewares/getTalkers');
+const talkerIdFind = require('./middlewares/talkerIdFind');
+const tokenLogin = require('./middlewares/tokenLogin');
 const {
   validateAge,
   validateName,
   validateAut,
   validateTalk,
-} = require("./middlewares/newTalker");
+} = require('./middlewares/newTalker');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = "3000";
+const PORT = '3000';
 
 // requisito 1
-app.get("/talker", getTalkers);
+app.get('/talker', getTalkers);
 // requisito 2
-app.get("/talker/:id", talkerIdFind);
+app.get('/talker/:id', talkerIdFind);
 // requisito 3
-app.post("/login", tokenLogin);
+app.post('/login', tokenLogin);
 // requisito 4
-app.post("/talker", validateAut, validateName, validateAge, validateTalk);
+app.post('/talker', validateAut, validateName, validateAge, validateTalk);
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get("/", (_request, response) => {
+app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
 app.listen(PORT, () => {
-  console.log("Online");
+  console.log('Online');
 });
--- middlewares/getTalkers.js
+++ middlewares/getTalkers.js
@@ -1,4 +1,4 @@
-const fs = require("fs/promises");
+const fs = require('fs/promises');
 const getTalkers = async (req, res, next) => {
   const talker = await fs.readFile("./talker.json", "utf8");
   if (talker === undefined) return res.status(200).json([]);
