971d3925142f825f70ba258f08310b2e42665705
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-12-07 20:02:53 -0300

    lint 2

--- middlewares/talkerIdFind.js
+++ middlewares/talkerIdFind.js
@@ -1,12 +1,12 @@
-const fs = require("fs/promises");
+const fs = require('fs/promises');
 
 const talkerIdFind = async (req, res, next) => {
   const { id } = req.params;
-  const talker = await fs.readFile("./talker.json", "utf8");
+  const talker = await fs.readFile('./talker.json', 'utf8');
   const jsonTalker = await JSON.parse(talker);
   const talkerFind = await jsonTalker.find((talk) => talk.id === Number(id));
   if (talkerFind) return res.status(200).json(talkerFind);
-  return res.status(404).json({ message: "Pessoa palestrante não encontrada" });
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   next();
 };
 
