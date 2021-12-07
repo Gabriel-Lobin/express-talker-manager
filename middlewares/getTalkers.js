a67933ecc448c9ec6e98cfb5ba8e861501bfaeb4
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-12-07 15:54:47 -0300

    [requisito 1] refatoracao

---
+++ middlewares/getTalkers.js
@@ -0,0 +1,10 @@
+const fs = require("fs/promises");
+const getTalkers = async (req, res, next) => {
+  const talker = await fs.readFile("./talker.json", "utf8");
+  if (talker === undefined) return res.status(200).json([]);
+  const retorno = JSON.parse(talker);
+  return res.status(200).json(retorno);
+  next();
+};
+
+module.exports = getTalkers;
