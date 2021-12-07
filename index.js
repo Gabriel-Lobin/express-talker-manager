36a71b0a5ae3b7e51329013a488568847d7cb5ff
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-12-07 15:54:29 -0300

    [requisito 1] refatoracao

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs/promises");
+const getTalkers = require("./middlewares/getTalkers");
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,14 +9,9 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = "3000";
 
-app.get("/talker", async (req, res, next) => {
-  const talker = await fs.readFile("./talker.json", 'utf8');
-  if(talker === undefined) return res.status(200).json([])
-  const retorno = JSON.parse(talker);
-  console.log(retorno);
-  return res.status(200).json(retorno);
-  next();
-});
+// requisito 1
+app.get("/talker", getTalkers);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get("/", (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
