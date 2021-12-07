728e1bd5808ef18a60cb38d928c5c9be25ee08e3
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-12-07 19:57:16 -0300

    [requisito 4] lint part 2 correct

--- middlewares/getTalkers.js
+++ middlewares/getTalkers.js
@@ -1,6 +1,6 @@
-const fs = require("fs/promises");
+const fs = require('fs/promises');
 const getTalkers = async (req, res, next) => {
-  const talker = await fs.readFile("./talker.json", "utf8");
+  const talker = await fs.readFile('./talker.json', 'utf8');
   if (talker === undefined) return res.status(200).json([]);
   const retorno = JSON.parse(talker);
   return res.status(200).json(retorno);
